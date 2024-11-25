15e8e82ce5c03d04c6cd029cd4e4026503f5db7b
Juan Felipe Mena <menavegajuanfe2005@gmail.com>
2024-11-25 18:22:59 +0100

    Modificación 1

--- src/JFM.java
+++ src/JFM.java
@@ -1,6 +1,7 @@
 public class JFM {
 	    public static void main(String[] args) {
 	        System.out.println("Alumno: JuanFelipe_Mena");
+	        System.out.println("Primera Modificación desde Master");
 	    }	
 }
 
